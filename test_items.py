083fc8c63978cada113d886542e940d2be5cc0ed
Епифанова Ольга Александровна <Olga.Epifanova@norbit.ru>
2023-05-31 18:47:33 +0300

    1

--- test_items.py
+++ test_items.py
@@ -5,6 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_language_check_button(browser):
     browser.get(link)
-    time.sleep(30)
     button=browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button, "Кнопка добавления в корзину не найдена"
